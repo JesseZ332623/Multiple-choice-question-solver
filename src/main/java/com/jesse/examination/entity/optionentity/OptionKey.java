76c2666df46c340b755b9b957a35354c8905d83a
浩进 朱 <3191955858@qq.com>
2025-04-08 18:01:54 +0800

    选项枚举类

---
+++ src/main/java/com/jesse/examination/entity/optionentity/OptionKey.java
@@ -0,0 +1,18 @@
+package com.jesse.examination.entity.optionentity;
+
+import lombok.Getter;
+
+/**
+ * 选项枚举类。
+ */
+@Getter
+public enum OptionKey
+{
+    A("A"), B("B"), C("C"), D("D");
+
+    private final String option;
+
+    OptionKey(String option) {
+        this.option = option;
+    }
+}
