900cc5ee4ba74767bc625cc38c65621c16103174
浩进 朱 <3191955858@qq.com>
2025-04-08 18:01:44 +0800

    数据表 options 的复合主键类

---
+++ src/main/java/com/jesse/examination/entity/optionentity/OptionID.java
@@ -0,0 +1,29 @@
+package com.jesse.examination.entity.optionentity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+/**
+ * 数据表 options 的复合主键类（被 @Embeddable 注解），
+ * 实现 Serializable 接口表明这个类可以被序列化。
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Embeddable
+public class OptionID implements Serializable
+{
+    @Column(name = "question_id")
+    private Integer questionId;
+
+    @Column(name = "option_key")
+    @Enumerated(value = EnumType.STRING)
+    private OptionKey optionKey;
+}
