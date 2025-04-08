946b639675d12025dc5c7d6458762037fbef4ba3
浩进 朱 <3191955858@qq.com>
2025-04-08 18:07:16 +0800

    练习成绩记录 CURD 操作 Restful 控制器类。

---
+++ src/main/java/com/jesse/examination/controller/restcontroller/ScoreRecordController.java
@@ -0,0 +1,143 @@
+package com.jesse.examination.controller.restcontroller;
+
+import com.jesse.examination.entity.scorerecord.ScoreRecordEntity;
+import com.jesse.examination.service.scorerecordservice.ScoreRecordService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+
+@RestController
+@RequestMapping(path = "/api/score_record")
+public class ScoreRecordController
+{
+    private final ScoreRecordService scoreRecordService;
+
+    @Autowired
+    public ScoreRecordController(ScoreRecordService scoreRecordService) {
+        this.scoreRecordService = scoreRecordService;
+    }
+
+    /*
+     *  GET 方法请求，通过指定的时间信息查询对应的成绩记录行，
+     *  前后端时间信息遵循 ISO 标准且精确到秒。
+     *
+     *  示例：2025-04-08T09:12:04
+     *
+     *  服务器以 JSON 格式作为响应（如果期间出现错误，会以 500 作为响应码）。
+     *
+     *  可能的 URL 为：http://localhost:8081/api/score_record/2025-04-08T09:12:04
+     */
+    @GetMapping(path = "/{dateTime}")
+    public ResponseEntity<?> getOneScoreRecord(
+            @PathVariable(name = "dateTime") LocalDateTime dateTime
+    )
+    {
+        try
+        {
+            ScoreRecordEntity scoreRecord
+                    = this.scoreRecordService.findScoreRecordByDateTime(dateTime);
+
+            return ResponseEntity.ok(scoreRecord);
+        }
+        catch (Exception runtimeException)
+        {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                                 .body(runtimeException.getMessage());
+        }
+    }
+
+    /*
+    * Get 方法请求，获取数据库中所有的练习成绩记录。
+    * 服务器以 JSON 格式作为响应（如果期间出现错误，会以 500 作为响应码）。
+    *
+    * 可能的 URL 为：http://localhost:8081/api/score_record/all_score_record
+    */
+    @GetMapping(path = "/all_score_record")
+    public ResponseEntity<?> getAllScoreRecord() {
+        return ResponseEntity.ok(this.scoreRecordService.findAllScoreRecord());
+    }
+
+    /*
+     * Post 方法请求，往数据表中添加一条新的练习记录。
+     * 如果需要手动提交进行测试的话，JSON 格式示例如下：
+     *
+     *
+     * {
+     *     "submitDate": "2015-01-12T19:12:37",
+     *     "correctCount": 100,
+     *     "errorCount": 20,
+     *     "noAnswerCount": 30,
+     *     "mistakeRate": 13.3333333
+     * }
+     *
+     *
+     * 服务器以 JSON 格式作为响应（如果期间出现错误，会以 500 作为响应码）。
+     *
+     * 可能的 URL 为：http://localhost:8081/api/score_record/add_one_new_score_record"
+     */
+    @PostMapping(path = "/add_one_new_score_record")
+    public ResponseEntity<?> addOneNewScoreRecord(
+            @RequestBody
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+            ScoreRecordEntity scoreRecord
+    )
+    {
+        try
+        {
+            LocalDateTime insertFeedBack
+                    = this.scoreRecordService.addNewScoreRecord(scoreRecord);
+
+            return ResponseEntity.ok()
+                                 .body(
+                                         String.format(
+                                                 "Insert New Score Record Complete, Record Submit time: %s",
+                                                 insertFeedBack.toString()
+                                         )
+                                 );
+        }
+        catch (Exception exception)
+        {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                                 .body(exception.getMessage());
+        }
+    }
+
+    /*
+     * Delete 方法请求，通过指定的时间信息删除对应的成绩记录行。
+     * 前后端时间信息遵循 ISO 标准且精确到秒。
+     *
+     * 示例：2025-04-08T09:12:04
+     *
+     * 服务器以 JSON 格式作为响应（如果期间出现错误，会以 500 作为响应码）。
+     *
+     * 可能的 URL 为：http://localhost:8081/api/score_record/2025-04-08T09:12:04"
+     */
+    @DeleteMapping(path = "/{dateTime}")
+    public ResponseEntity<?> deleteOneScoreRecord(
+            @PathVariable(name = "dateTime") LocalDateTime dateTime
+    )
+    {
+        try
+        {
+            LocalDateTime deleteFeedBack
+                    = this.scoreRecordService.deleteScoreRecordByDate(dateTime);
+
+            return ResponseEntity.ok()
+                                 .body(
+                                         String.format(
+                                                 "Delete Score Record Complete, Record Submit time: %s",
+                                                 deleteFeedBack.toString()
+                                         )
+                                 );
+        }
+        catch (Exception exception)
+        {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body(exception.getMessage());
+        }
+    }
+}
