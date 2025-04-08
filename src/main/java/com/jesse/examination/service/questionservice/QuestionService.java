f1ed49948749c764853a29d0f15f77f93aa9a989
浩进 朱 <3191955858@qq.com>
2025-04-08 18:05:02 +0800

    问题服务类接口。

---
+++ src/main/java/com/jesse/examination/service/questionservice/QuestionService.java
@@ -0,0 +1,27 @@
+package com.jesse.examination.service.questionservice;
+
+import com.jesse.examination.DTO.QuestionInfoDTO;
+import com.jesse.examination.DTO.QuestionWithCorrectOptionDTO;
+
+import java.util.List;
+
+public interface QuestionService
+{
+    /**
+     * 查询 question 表中的所有问题和正确选项，
+     * 以及 options  表中对应的所有选项内容。
+     */
+    List<QuestionInfoDTO> getAllQuestionInfo();
+
+    /**
+     * 查询 question 表中的所有问题和正确选项，
+     * 以及 options  表中对应的正确选项内容。
+     */
+    List<QuestionWithCorrectOptionDTO> getAllQuestionWithCorrectOption();
+
+    /**
+     * 将 questions 表中指定 id 对应的数据行的 current_times 的值 + 1，
+     * 表明用户答对了这道题一次。
+     */
+    Integer correctTimesPlusOneById(Integer id);
+}
