42a6f02f6261427a7844db6300bc3fe532c7fa09
浩进 朱 <3191955858@qq.com>
2025-04-11 15:46:15 +0800

    练习成绩记录仓库类接口。

--- src/main/java/com/jesse/examination/repository/ScoreRecordRepository.java
+++ src/main/java/com/jesse/examination/repository/ScoreRecordRepository.java
@@ -2,9 +2,17 @@ package com.jesse.examination.repository;
 
 import com.jesse.examination.entity.scorerecord.ScoreRecordEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.Optional;
 
-public interface ScoreRecordRepository extends JpaRepository<ScoreRecordEntity, LocalDateTime> {}
+public interface ScoreRecordRepository extends JpaRepository<ScoreRecordEntity, LocalDateTime>
+{
+    /**
+     * 清空 score_record 表。
+     */
+    @Modifying
+    @Query(value = "TRUNCATE score_record", nativeQuery = true)
+    void truncateScoreRecordTable();
+}
