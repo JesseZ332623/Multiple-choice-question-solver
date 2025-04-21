dfc7e047ae949aa796f400a0d491abab666b6169
浩进 朱 <3191955858@qq.com>
2025-04-21 10:30:48 +0800

    练习成绩记录仓库类接口。

--- src/main/java/com/jesse/examination/scorerecord/repository/ScoreRecordRepository.java
+++ src/main/java/com/jesse/examination/scorerecord/repository/ScoreRecordRepository.java
@@ -1,13 +1,13 @@
-package com.jesse.examination.repository;
+package com.jesse.examination.scorerecord.repository;
 
-import com.jesse.examination.entity.scorerecord.ScoreRecordEntity;
+import com.jesse.examination.scorerecord.entity.ScoreRecordEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import java.time.LocalDateTime;
 
-public interface ScoreRecordRepository extends JpaRepository<ScoreRecordEntity, LocalDateTime>
+public interface ScoreRecordRepository extends JpaRepository<ScoreRecordEntity, Integer>
 {
     /**
      * 清空 score_record 表。
