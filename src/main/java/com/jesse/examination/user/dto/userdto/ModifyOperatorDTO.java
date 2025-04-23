b236f6a477abd683314e1db450a021526cc3d651
Jesse <3191955858@qq.com>
2025-04-23 22:30:24 +0800

    用户修改账户信息所用的 DTO。

---
+++ src/main/java/com/jesse/examination/user/dto/userdto/ModifyOperatorDTO.java
@@ -0,0 +1,10 @@
+package com.jesse.examination.user.dto.userdto;
+
+import lombok.Data;
+
+@Data
+public class ModifyOperatorDTO
+{
+    private UserLoginDTO  userLoginDTO;
+    private UserModifyDTO userMidifyInfoDTO;
+}
