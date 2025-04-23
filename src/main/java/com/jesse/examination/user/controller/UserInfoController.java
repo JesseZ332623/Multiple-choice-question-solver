97e99528751da4a8ef4469f21a7804ec8b720422
Jesse <3191955858@qq.com>
2025-04-23 22:33:52 +0800

    用户注册，登录，修改，注销账户控制器类。

---
+++ src/main/java/com/jesse/examination/user/controller/UserInfoController.java
@@ -0,0 +1,121 @@
+package com.jesse.examination.user.controller;
+
+import com.jesse.examination.user.dto.userdto.ModifyOperatorDTO;
+import com.jesse.examination.user.dto.userdto.UserLoginDTO;
+import com.jesse.examination.user.dto.userdto.UserRegistrationDTO;
+import com.jesse.examination.user.service.UserServiceInterface;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import static java.lang.String.format;
+
+@Slf4j
+@RestController
+@RequestMapping(path = "/api/user_info/")
+public class UserInfoController
+{
+    private final UserServiceInterface userServiceInterface;
+
+    @Autowired
+    public UserInfoController(UserServiceInterface userServiceInterface) {
+        this.userServiceInterface = userServiceInterface;
+    }
+
+    @PostMapping(path = "register")
+    public ResponseEntity<?> doRegister(
+            @RequestBody
+            UserRegistrationDTO userRegistrationDTO)
+    {
+        try
+        {
+            this.userServiceInterface.userRegister(userRegistrationDTO);
+
+            return ResponseEntity.ok(
+                    format(
+                            "Register complete. Welcome User: [%s]!",
+                            userRegistrationDTO.getUserName()
+                    )
+            );
+        }
+        catch (Exception exception)
+        {
+            log.error(exception.getMessage());
+
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                                 .body(exception.getMessage());
+        }
+    }
+
+    @PostMapping(path = "login")
+    public ResponseEntity<?> doLogin(
+            @RequestBody
+            UserLoginDTO userLoginDTO)
+    {
+        try
+        {
+            this.userServiceInterface.userLogin(userLoginDTO);
+
+            return ResponseEntity.ok(
+                    format(
+                            "Login complete. Welcome %s!",
+                            userLoginDTO.getUserName()
+                    )
+            );
+        }
+        catch (Exception exception)
+        {
+            log.error(exception.getMessage());
+
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                                 .body(exception.getMessage());
+        }
+    }
+
+    @PutMapping(path = "modify")
+    public ResponseEntity<?> doModify(
+            @RequestBody ModifyOperatorDTO modifyOperatorDTO)
+    {
+        try
+        {
+            this.userServiceInterface.modifyUserInfo(
+                    modifyOperatorDTO.getUserLoginDTO(),
+                    modifyOperatorDTO.getUserMidifyInfoDTO()
+            );
+
+            return ResponseEntity.ok("Modify complete!");
+        }
+        catch (Exception exception)
+        {
+            log.error(exception.getMessage());
+
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body(exception.getMessage());
+        }
+    }
+
+    @DeleteMapping(path = "delete")
+    public ResponseEntity<?> doDelete(@RequestBody UserLoginDTO userLoginDTO)
+    {
+        try
+        {
+            this.userServiceInterface.deleteUser(userLoginDTO);
+
+            return ResponseEntity.ok(
+                    format(
+                            "Delete complete! See you again [%s]!",
+                            userLoginDTO.getUserName()
+                    )
+            );
+        }
+        catch (Exception exception)
+        {
+            log.error(exception.getMessage());
+
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body(exception.getMessage());
+        }
+    }
+}
