c4fe74f2aea73343843a86dde4950fd3de7ffebd
浩进 朱 <3191955858@qq.com>
2025-04-24 19:31:59 +0800

    普通用户的首页（有别于管理员）。

--- src/main/java/com/jesse/examination/user/controller/frontpage/FrontPageViewController.java
+++ src/main/java/com/jesse/examination/user/controller/frontpage/FrontPageViewController.java
@@ -1,10 +1,10 @@
-package com.jesse.examination.frontpage;
+package com.jesse.examination.user.controller.frontpage;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping("/front_page")
+@RequestMapping("/user_info")
 @Controller
 public class FrontPageViewController
 {
@@ -13,11 +13,13 @@ public class FrontPageViewController
      *
      * <p>
      *     链接：
-     *     <a href="https://localhost:8081/front_page">
+     *     <a href="https://localhost:8081/user_info/front_page">
      *         (GET Method) 跳转至首页。
      *     </a>
      * </p>
      */
-    @GetMapping
-    public String FrontPageView() { return "FrontPage"; }
+    @GetMapping(path = "/user_front_page")
+    public String FrontPageView() {
+        return "UserAccountPage/UserFrontPage";
+    }
 }
