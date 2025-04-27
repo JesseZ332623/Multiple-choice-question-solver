fb9a5b695d2dfd9eedb6d8c0484fae49ba23eee3
浩进 朱 <3191955858@qq.com>
2025-04-27 21:07:11 +0800

    网络安全配置类（未完成）。

--- src/main/java/com/jesse/examination/config/securityconfig/WebSecurityConfiguration.java
+++ src/main/java/com/jesse/examination/config/securityconfig/WebSecurityConfiguration.java
@@ -12,8 +12,29 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class WebSecurityConfiguration
 {
-    private static final String[] permitURLs = {
-            "/**"
+    private static final String[] PUBLIC_URLS = {
+            "/user_info/login",
+            "/user_info/register"
+    };
+
+    private static final String[] USERS_PERMIT_URLS = {
+            "/api/question/**",
+            "/api/score_record/**",
+            "/api/user_info/**",
+            "/question/**",
+            "/score_record/**",
+            "/user_info/**",
+            "/error/**"
+    };
+
+    private static final String[] ADMIN_PERMIT_URLS = {
+            "/api/question/**",
+            "/api/score_record/**",
+            "/api/user_info/**",
+            "/api/admin/**",
+            "/question/**",
+            "/score_record/**",
+            "/user_info/**"
     };
 
     @Bean
@@ -24,11 +45,24 @@ public class WebSecurityConfiguration
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception
     {
-         http.authorizeHttpRequests(
-                (auth) ->
-                                auth.requestMatchers(permitURLs).permitAll()
-                                .anyRequest().authenticated()
-        ).csrf(AbstractHttpConfigurer::disable);
+//         http.authorizeHttpRequests(
+//                (auth) ->
+//                        auth.requestMatchers(PUBLIC_URLS).permitAll()
+//                            .requestMatchers(USERS_PERMIT_URLS).hasAnyRole("USER", "ADMIN")
+//                            .requestMatchers(ADMIN_PERMIT_URLS).hasRole("ADMIN")
+//                            .anyRequest().authenticated()
+//        ).formLogin(
+//                (form) ->
+//                        form.loginPage("/user_info/login")
+//                            .permitAll()
+//         ).exceptionHandling(
+//                 (handle) ->
+//                         handle.accessDeniedPage("/error/access_denied")
+//         );
+
+        http.authorizeHttpRequests((auth) ->
+                auth.requestMatchers("/**").permitAll()
+        );// .csrf(AbstractHttpConfigurer::disable);
 
         return http.build();
     }
