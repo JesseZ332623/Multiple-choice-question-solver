e5bada0501784f489abbef611bd97c534a8282be
浩进 朱 <3191955858@qq.com>
2025-04-27 21:01:07 +0800

    管理员用户操作服务接口

--- src/main/java/com/jesse/examination/user/service/AdminServiceInterface.java
+++ src/main/java/com/jesse/examination/user/service/AdminServiceInterface.java
@@ -4,9 +4,13 @@ import com.jesse.examination.user.dto.admindto.AdminAddNewUserDTO;
 import com.jesse.examination.user.dto.admindto.AdminModifyUserDTO;
 import com.jesse.examination.user.entity.UserEntity;
 
+import java.util.List;
+
 public interface AdminServiceInterface
 {
     UserEntity findUserByUserName(String userName);
+
+    List<UserEntity> findAllUsers();
 
     Long addNewUser(AdminAddNewUserDTO newUser);
 
