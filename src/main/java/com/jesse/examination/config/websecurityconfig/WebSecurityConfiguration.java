3a0eb45212e1c9f333d20b76d0c066897da21fb8
浩进 朱 <3191955858@qq.com>
2025-04-28 17:07:57 +0800

    应用程序网络安全配置类。

--- src/main/java/com/jesse/examination/config/websecurityconfig/WebSecurityConfiguration.java
+++ src/main/java/com/jesse/examination/config/websecurityconfig/WebSecurityConfiguration.java
@@ -1,10 +1,9 @@
-package com.jesse.examination.config.securityconfig;
+package com.jesse.examination.config.websecurityconfig;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
