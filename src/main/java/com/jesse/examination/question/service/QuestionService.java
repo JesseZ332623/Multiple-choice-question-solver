b15f57bc504fa916066269be8616c3e7c533aa8a
浩进 朱 <3191955858@qq.com>
2025-05-02 21:17:33 +0800

    选择题问题服务类接口。

--- src/main/java/com/jesse/examination/question/service/QuestionService.java
+++ src/main/java/com/jesse/examination/question/service/QuestionService.java
@@ -1,9 +1,12 @@
 package com.jesse.examination.question.service;
 
+import com.jesse.examination.question.dto.QuestionCorrectTimesDTO;
 import com.jesse.examination.question.dto.QuestionInfoDTO;
 import com.jesse.examination.question.dto.QuestionWithCorrectOptionDTO;
+import org.springframework.data.domain.Limit;
 
 import java.util.List;
+import java.util.Map;
 
 public interface QuestionService
 {
@@ -12,6 +15,11 @@ public interface QuestionService
      * 以及 options  表中对应的所有选项内容。
      */
     List<QuestionInfoDTO> getAllQuestionInfo();
+
+    /**
+     * 获取所有问题的答对次数，存储在一个不可变列表中。
+     */
+    List<QuestionCorrectTimesDTO> getAllQuestionCorrectTimes();
 
     /**
      * 查询 question 表中的所有问题和正确选项，
@@ -25,5 +33,19 @@ public interface QuestionService
      */
     Integer correctTimesPlusOneById(Integer id);
 
+    /**
+     * 将 questions 表中指定 id 对应的数据行的
+     * current_times 的值设为 value。
+     */
+     void setOneCorrectTimesById(Integer id, Integer value);
+
+    /***/
+    void setAllCorrectTimesByIds(
+            List<QuestionCorrectTimesDTO> correctTimesDTOList
+    );
+
+    /**
+     * 将问题的答对次数全部设为 0
+     */
     Integer clearCorrectTimesToZero();
 }
