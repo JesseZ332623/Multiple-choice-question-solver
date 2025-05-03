ad35e255d60e403d1b541e1e0c6f6e647a470d5c
Jesse <3191955858@qq.com>
2025-05-03 22:11:19 +0800

    Redis 数据库配置类。

--- src/main/java/com/jesse/examination/config/redisconfig/RedisConfig.java
+++ src/main/java/com/jesse/examination/config/redisconfig/RedisConfig.java
@@ -1,8 +1,12 @@
 package com.jesse.examination.config.redisconfig;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -14,8 +18,25 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 public class RedisConfig
 {
     @Bean
+    public RedisConnectionFactory redisConnectionFactory(
+            @Value("${spring.redis.host}")      String host,
+            @Value("${spring.redis.port}")      int port,
+            @Value("${spring.redis.password}")  String password
+    )
+    {
+        var conf = new RedisStandaloneConfiguration(host, port);
+
+        conf.setPassword(password);
+
+        return new LettuceConnectionFactory(conf);
+    }
+
+    @Bean
     public RedisTemplate<String, Object>
-    configRedisTemplate(RedisConnectionFactory connectionFactory)
+    configRedisTemplate(
+            @Qualifier(value = "redisConnectionFactory")
+            RedisConnectionFactory connectionFactory
+    )
     {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
