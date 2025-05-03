3553ab338ba0fec056a555ee157181ec3056f716
Jesse <3191955858@qq.com>
2025-05-03 22:12:01 +0800

    score_record 数据表实体类。

--- src/main/java/com/jesse/examination/scorerecord/entity/ScoreRecordEntity.java
+++ src/main/java/com/jesse/examination/scorerecord/entity/ScoreRecordEntity.java
@@ -1,7 +1,11 @@
 package com.jesse.examination.scorerecord.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
@@ -9,6 +13,8 @@ import java.time.LocalDateTime;
  * score_record 数据表实体类。
  */
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "score_record")
 public class ScoreRecordEntity
@@ -16,31 +22,38 @@ public class ScoreRecordEntity
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "score_id")
+    @JsonProperty("scoreId")
     private Integer scoreId;                // 成绩记录 ID
 
     @Column(name = "submit_date")
+    @JsonProperty("submitDate")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime submitDate;        // 成绩提交日期
 
     @Column(name = "correct_count")
+    @JsonProperty("correctCount")
     private Integer correctCount;           // 正确数
 
     @Column(name = "error_count")
+    @JsonProperty("errorCount")
     private Integer errorCount;             // 错误数
 
     @Column(name = "no_answer_count")
+    @JsonProperty("noAnswerCount")
     private Integer noAnswerCount;          // 未答数
 
     @Column(name = "mistake_rate")
-    private Double mistakeRate;             // 错误率（这个也要存？）
+    @JsonProperty("mistakeRate")
+    private Double mistakeRate;             // 错误率
 
     @Override
     public String toString()
     {
         return String.format(
                 """
-                {"scoreId" : %d, "submitDate" : "%s", "correctCount" : %d, "errorCount" : %d, "noAnswerCount" : %d, "mistakeRate" : %f}
+                {"submitDate" : "%s", "correctCount" : %d, "errorCount" : %d, "noAnswerCount" : %d, "mistakeRate" : %f}
                 """,
-                scoreId, submitDate.toString(), correctCount,
+                submitDate.toString(), correctCount,
                 errorCount, noAnswerCount, mistakeRate
         );
     }
