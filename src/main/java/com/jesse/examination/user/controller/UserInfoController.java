3d9b4dead95d5cbc7abd338620d8ba3a6b253d06
浩进 朱 <3191955858@qq.com>
2025-05-06 19:54:21 +0800

    用户注册，登录，修改，注销账户控制器类。

--- src/main/java/com/jesse/examination/user/controller/UserInfoController.java
+++ src/main/java/com/jesse/examination/user/controller/UserInfoController.java
@@ -9,9 +9,12 @@ import com.jesse.examination.user.dto.userdto.ModifyOperatorDTO;
 import com.jesse.examination.user.dto.userdto.UserLoginDTO;
 import com.jesse.examination.user.dto.userdto.UserRegistrationDTO;
 import com.jesse.examination.user.service.UserServiceInterface;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -26,25 +29,22 @@ import static java.lang.String.format;
 @RequestMapping(path = "/api/user_info/", produces = "application/json")
 public class UserInfoController
 {
-    private final UserServiceInterface          userServiceInterface;
+    private final UserServiceInterface          userService;
     private final QuestionService               questionService;
     private final ScoreRecordService            scoreRecordService;
-    private final RedisTemplate<String, String> redisTemplate;
     private final FileTransferService           fileTransferService;
 
     @Autowired
     public UserInfoController(
-            UserServiceInterface            userServiceInterface,
+            UserServiceInterface            userService,
             QuestionService                 questionService,
             ScoreRecordService              scoreRecordService,
-            RedisTemplate<String, String>   redisTemplate,
             FileTransferService             fileTransferService
     )
     {
-        this.userServiceInterface = userServiceInterface;
+        this.userService          = userService;
         this.questionService      = questionService;
         this.scoreRecordService   = scoreRecordService;
-        this.redisTemplate        = redisTemplate;
         this.fileTransferService  = fileTransferService;
     }
 
@@ -55,7 +55,7 @@ public class UserInfoController
     {
         try
         {
-            this.userServiceInterface.userRegister(userRegistrationDTO);
+            this.userService.userRegister(userRegistrationDTO);
 
             return ResponseEntity.ok(
                     format(
@@ -75,29 +75,43 @@ public class UserInfoController
 
     @PostMapping(path = "login")
     public ResponseEntity<?> doLogin(
-            @RequestBody
-            UserLoginDTO userLoginDTO)
+            @RequestBody UserLoginDTO userLoginDTO,
+            HttpServletRequest        request,
+            HttpServletResponse       response
+    )
     {
         try
         {
-            this.userServiceInterface.userLogin(userLoginDTO);
+            this.userService.userLogin(userLoginDTO);
 
-            this.redisTemplate.opsForValue().set(
-                    "user:UserInfoController:login_username",
-                    userLoginDTO.getUserName()
-            );
+            /*
+             * 为了防止固定会话攻击，每次登录都会强制创建新的 Session。
+             */
+            HttpSession session = request.getSession(false);
+            if (!Objects.equals(session, null)) {
+                session.invalidate();
+            }
+
+            // 将用户名和这个 session 相关联。
+            session = request.getSession(true);
+            session.setAttribute("user", userLoginDTO.getUserName());
+
+            // 设置 Cookie 确保浏览器使用新会话 ID
+            Cookie cookie = new Cookie("JSESSIONID", session.getId());
+            cookie.setPath("/");
+            cookie.setHttpOnly(true);
+            cookie.setSecure(true);         // 如果使用 HTTPS
+            response.addCookie(cookie);
 
             List<QuestionCorrectTimesDTO> questionCorrectTimesDTOS
                     = this.fileTransferService.readUserCorrectTimesDataFile(
-                                this.redisTemplate.opsForValue()
-                                        .get("user:UserInfoController:login_username")
-                        );
+                        userLoginDTO.getUserName()
+                    );
 
             List<ScoreRecordEntity> scoreRecordEntityList
                     = this.fileTransferService.readUserScoreDataFile(
-                    this.redisTemplate.opsForValue()
-                            .get("user:UserInfoController:login_username")
-            );
+                            userLoginDTO.getUserName()
+                    );
 
 //            for (var n : questionCorrectTimesDTOS) {
 //                System.out.println(n);
@@ -114,6 +128,11 @@ public class UserInfoController
             if (!scoreRecordEntityList.isEmpty()) {
                 this.scoreRecordService.saveScoreRecordFromList(scoreRecordEntityList);
             }
+
+            log.info(
+                    "User [{}] logged in with session ID: {}",
+                    userLoginDTO.getUserName(), session.getId()
+            );
 
             return ResponseEntity.ok(
                     format(
@@ -132,20 +151,31 @@ public class UserInfoController
     }
 
     @PostMapping(path = "logout")
-    public ResponseEntity<String> doLogout()
+    public ResponseEntity<String> doLogout(
+            HttpServletRequest  request,
+            HttpServletResponse response
+    )
     {
         try
         {
-            String logoutUserName
-                    = Objects.requireNonNull(
-                    this.redisTemplate.opsForValue()
-                            .get("user:UserInfoController:login_username")
-            );
+            HttpSession session = request.getSession(false);
+            String logoutUserName = null;
 
-            String responseText
-                    = format("User [%s] logout, see you later~", logoutUserName);
+            if (!Objects.equals(session, null))
+            {
+                logoutUserName = (String) session.getAttribute("user");
+                session.invalidate();
+            }
 
-            log.info(responseText);
+            // 创建一个名为 "JSESSIONID" 的新 Cookie，值设为 null
+            Cookie cookie = new Cookie("JSESSIONID", null);
+            cookie.setPath("/");        // 路径必须与登录时设置的路径一致
+            cookie.setSecure(true);
+            cookie.setHttpOnly(true);
+            cookie.setMaxAge(0);        // 立即过期
+            response.addCookie(cookie); // 将新的 Cookie 添加到响应中
+
+            log.info("User [{}] logout, see you later~", logoutUserName);
 
             // 存档用户数据
             List<QuestionCorrectTimesDTO> questionCorrectTimesList
@@ -154,13 +184,13 @@ public class UserInfoController
             log.info("Call getAllQuestionCorrectTimes() complete.");
 
             List<ScoreRecordEntity> allScoreList
-                    = this.scoreRecordService.findAllScoreRecord();
+                    = this.scoreRecordService.findAllScoreRecordByUserName(logoutUserName);
 
             log.info("Call findAllScoreRecord complete.");
 
             // 清空指定的数据
             this.questionService.clearCorrectTimesToZero();
-            this.scoreRecordService.truncateScoreRecordTable();
+            // this.scoreRecordService.truncateScoreRecordTable();
 
             log.info("truncate user data complete.");
 
@@ -174,11 +204,13 @@ public class UserInfoController
 
             log.info("save date to file complete.");
 
-            this.redisTemplate.delete("user:UserInfoController:login_username");
-
-            log.info("delete user redis data complete.");
-
-            return ResponseEntity.ok().body(responseText);
+            return ResponseEntity.ok()
+                    .body(
+                            format(
+                                    "User [%s] logout, see you later~",
+                                    logoutUserName
+                            )
+                    );
         }
         catch (Exception exception)
         {
@@ -193,7 +225,7 @@ public class UserInfoController
     {
         try
         {
-            this.userServiceInterface.modifyUserInfo(modifyOperatorDTO);
+            this.userService.modifyUserInfo(modifyOperatorDTO);
 
             return ResponseEntity.ok(
                     format(
@@ -216,7 +248,7 @@ public class UserInfoController
     {
         try
         {
-            this.userServiceInterface.deleteUser(userLoginDTO);
+            this.userService.deleteUser(userLoginDTO);
 
             return ResponseEntity.ok(
                     format(
