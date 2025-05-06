d138ac7d9c419fc0a357c6f669e83a836eb9eeed
浩进 朱 <3191955858@qq.com>
2025-05-06 09:07:56 +0800

    Jackson 配置类

--- src/main/java/com/jesse/examination/config/jacksonconfig/JacksonConfig.java
+++ src/main/java/com/jesse/examination/config/jacksonconfig/JacksonConfig.java
@@ -3,12 +3,16 @@ package com.jesse.examination.config.jacksonconfig;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
+/**
+ * 原生的 Jackson 库不支持对时间字符串（如 ISO 标准格式：2025-04-30T09:41:01）的转化，
+ * 所以需要一个工厂方法去构造注册了 JavaTime Module 的 ObjectMapper。
+ */
 public class JacksonConfig
 {
     public static ObjectMapper createObjectMapper()
     {
         ObjectMapper mapper = new ObjectMapper();
-        mapper.registerModule(new JavaTimeModule()); // 注册JavaTimeModule
+        mapper.registerModule(new JavaTimeModule()); // 注册 JavaTime Module
 
         return mapper;
     }
