809cbcfea899616368c51088508b1e53ba9067cb
浩进 朱 <3191955858@qq.com>
2025-05-06 19:45:28 +0800

    用户首页视图控制器。

--- src/main/java/com/jesse/examination/user/controller/ordinary_user_frontpage/FrontPageViewController.java
+++ src/main/java/com/jesse/examination/user/controller/ordinary_user_frontpage/FrontPageViewController.java
@@ -1,50 +1,55 @@
 package com.jesse.examination.user.controller.ordinary_user_frontpage;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.RedisTemplate;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.Optional;
+import java.util.Objects;
 
+@Slf4j
 @RequestMapping("/user_info")
 @Controller
 public class FrontPageViewController
 {
-    public final RedisTemplate<String, String> redisTemplate;
-
-    @Autowired
-    public FrontPageViewController(RedisTemplate<String, String> template) {
-        this.redisTemplate = template;
-    }
-
     /**
      * 跳转至普通用户操作首页。
      *
      * <p>
      *     链接：
      *     <a href="https://localhost:8081/user_info/front_page">
-     *         (GET Method) 跳转至首页。
+     *         (GET Method) 跳转至普通用户操作首页。
      *     </a>
      * </p>
      */
     @GetMapping(path = "/user_front_page")
-    public String FrontPageView(Model model)
+    public String FrontPageView(
+            Model model,
+            HttpServletRequest request
+    )
     {
-        Optional<String> value
-                = Optional.ofNullable(
-                this.redisTemplate.opsForValue().get(
-                        "user:UserInfoController:login_username"
-                )
-        );
+        try
+        {
+            HttpSession session = request.getSession(false);
 
-        if (value.isPresent()) {
-            model.addAttribute("UserName", value.get());
+            if (!Objects.equals(session, null))
+            {
+                model.addAttribute(
+                        "UserName",
+                        session.getAttribute("user")
+                );
+            }
         }
-        else {
-            model.addAttribute("UserName", "Unknow");
+        catch (Exception exception)
+        {
+            log.error(exception.getMessage());
+
+            model.addAttribute(
+                    "UserName", "Unknow"
+            );
         }
 
         return "UserOperatorPage/UserFrontPage";
