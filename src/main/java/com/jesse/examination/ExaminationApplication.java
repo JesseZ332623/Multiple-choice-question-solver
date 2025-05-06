ff7ddf7c9f7296f86f90a4e8a6933e10eca3e094
浩进 朱 <3191955858@qq.com>
2025-05-06 19:43:17 +0800

    应用程序启动类。

--- src/main/java/com/jesse/examination/ExaminationApplication.java
+++ src/main/java/com/jesse/examination/ExaminationApplication.java
@@ -2,8 +2,14 @@ package com.jesse.examination;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
+
+/*
+ * EnableRedisHttpSession 注解用于启用 Http 会话。
+ */
+@EnableRedisHttpSession
 public class ExaminationApplication
 {
 	public static void main(String[] args) {
