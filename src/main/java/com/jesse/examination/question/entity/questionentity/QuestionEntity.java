d3b2d93c4cb21e1ae3b79c1c9488d5048375f356
浩进 朱 <3191955858@qq.com>
2025-05-07 23:08:05 +0800

    question 数据表实体类。

--- src/main/java/com/jesse/examination/question/entity/questionentity/QuestionEntity.java
+++ src/main/java/com/jesse/examination/question/entity/questionentity/QuestionEntity.java
@@ -27,13 +27,6 @@ public class QuestionEntity
     @Enumerated(value = EnumType.STRING)
     private OptionKey answer;                               // 问题答案
 
-    @Column(name = "correct_times")
-    private Integer correctTimes;                           // 问题答对的次数
-
-//    // 该问题是否掌握（默认 false，答对 3 次之后改为 true）
-//    @Column(name = "enable")
-//    private Boolean enable;
-
     // 问题选项（一个问题有多个选项）
     @OneToMany(mappedBy = "question", fetch = FetchType.LAZY)
     private List<OptionEntity> options;
