98161d2222a9b38ede41874b1cfdb1de9d781d34
浩进 朱 <3191955858@qq.com>
2025-05-07 23:07:17 +0800

    问题详情 DTO。

--- src/main/java/com/jesse/examination/question/dto/QuestionInfoDTO.java
+++ src/main/java/com/jesse/examination/question/dto/QuestionInfoDTO.java
@@ -14,7 +14,6 @@ public class QuestionInfoDTO
     private Integer questionId;
     private String  questionContent;
     private String  correctAnswer;
-    private Integer correctTimes;
 
     // 解析后的选项（如 {"A": "内容", "B": "内容"}）
     public Map<String, Object> options;
