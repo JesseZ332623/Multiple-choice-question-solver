a1ac93d5d0a1596b4a3349492ca8f9d3e4be641e
浩进 朱 <3191955858@qq.com>
2025-05-07 23:05:31 +0800

    所有问题视图控制器。

--- src/main/java/com/jesse/examination/question/controller/AllQuestionViewController.java
+++ src/main/java/com/jesse/examination/question/controller/AllQuestionViewController.java
@@ -1,9 +1,13 @@
 package com.jesse.examination.question.controller;
 
+import com.jesse.examination.question.dto.QuestionCorrectTimesDTO;
 import com.jesse.examination.question.dto.QuestionInfoDTO;
 import com.jesse.examination.errorhandle.ControllerErrorMessage;
 import com.jesse.examination.errorhandle.ErrorMessageGenerator;
 import com.jesse.examination.question.service.QuestionService;
+import com.jesse.examination.redis.service.RedisServiceInterface;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -18,11 +22,17 @@ import java.util.List;
 @RequestMapping(path = "/question")
 public class AllQuestionViewController
 {
-    private final QuestionService questionService;
+    private final QuestionService       questionService;
+    private final RedisServiceInterface redisService;
 
     @Autowired
-    public AllQuestionViewController(QuestionService questionService) {
+    public AllQuestionViewController(
+            QuestionService         questionService,
+            RedisServiceInterface   redisService
+    )
+    {
         this.questionService = questionService;
+        this.redisService    = redisService;
     }
 
     /**
@@ -38,15 +48,32 @@ public class AllQuestionViewController
      * </p>
      */
     @GetMapping(path = "all_questions")
-    public String getAllQuestionView(Model model)
+    public String getAllQuestionView(
+            Model model,
+            HttpServletRequest request
+    )
     {
         try
         {
+            HttpSession session       = request.getSession(false);
+            String      loginUserName = (String) session.getAttribute("user");
+
             List<QuestionInfoDTO> allQuestionQueryResult
                     = this.questionService.getAllQuestionInfo();
 
+            List<QuestionCorrectTimesDTO> allQuestionCorrectTimes
+                        = this.redisService.readQuestionCorrectTimeList(loginUserName);
+
+            model.addAttribute("UserName", loginUserName);
+
             model.addAttribute(
-                    "AllQuestions", allQuestionQueryResult
+                    "AllQuestions",
+                    allQuestionQueryResult
+            );
+
+            model.addAttribute(
+                    "QuestionCorrectTimes",
+                    allQuestionCorrectTimes
             );
 
             return "UserOperatorPage/AllQuestions";
@@ -69,7 +96,7 @@ public class AllQuestionViewController
     }
 
     /**
-     * GET 方法请求，获取所有问题的内容，正确答案，答对的次数以及对应正确选项的内容。
+     * GET 方法请求，获取所有问题的内容，正确答案以及对应正确选项的内容。
      * 以视图作为响应，渲染在 AllQuestionsWithCorrectResult.html 页面中
      * 如果期间出现错误，会跳转到统一的 Controller_ErrorPage.html 页面并显示错误消息。
      *
