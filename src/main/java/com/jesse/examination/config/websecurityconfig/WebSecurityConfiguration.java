e38800ffc6696aebfb70d8db7e5633cdb7015444
浩进 朱 <3191955858@qq.com>
2025-05-08 09:16:16 +0800

    应用程序网络安全配置类。

--- src/main/java/com/jesse/examination/config/websecurityconfig/WebSecurityConfiguration.java
+++ src/main/java/com/jesse/examination/config/websecurityconfig/WebSecurityConfiguration.java
@@ -4,19 +4,23 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+/**
+ * Spring 网络安全配置类。
+ */
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfiguration
 {
+    // 所有人都可以访问的 URL
     private static final String[] PUBLIC_URLS = {
             "/user_info/login",
             "/user_info/register"
     };
 
+    // 管理员或仅拥有用户权限的人可访问的 URL
     private static final String[] USERS_PERMIT_URLS = {
             "/api/question/**",
             "/api/score_record/**",
@@ -27,6 +31,7 @@ public class WebSecurityConfiguration
             "/error/**"
     };
 
+    // 管理员可访问的 URL
     private static final String[] ADMIN_PERMIT_URLS = {
             "/api/question/**",
             "/api/score_record/**",
