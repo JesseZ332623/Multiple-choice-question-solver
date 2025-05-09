58e846b8f3499b34a321cb9bde39e9a0b6260117
浩进 朱 <3191955858@qq.com>
2025-05-09 18:11:43 +0800

    Spring 网络安全配置类（遇到一些阻碍，暂时停用）。

--- src/main/java/com/jesse/examination/config/websecurityconfig/WebSecurityConfiguration.java
+++ src/main/java/com/jesse/examination/config/websecurityconfig/WebSecurityConfiguration.java
@@ -8,7 +8,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 /**
- * Spring 网络安全配置类。
+ * Spring 网络安全配置类（遇到一些阻碍，暂时停用）。
  */
 @Configuration
 @EnableWebSecurity
@@ -16,13 +16,16 @@ public class WebSecurityConfiguration
 {
     // 所有人都可以访问的 URL
     private static final String[] PUBLIC_URLS = {
+            "/",
             "/user_info/login",
-            "/user_info/register"
+            "/user_info/register",
+            "/api/email/**"
     };
 
     // 管理员或仅拥有用户权限的人可访问的 URL
     private static final String[] USERS_PERMIT_URLS = {
             "/api/question/**",
+            "/api/redis/**",
             "/api/score_record/**",
             "/api/user_info/**",
             "/question/**",
@@ -34,12 +37,17 @@ public class WebSecurityConfiguration
     // 管理员可访问的 URL
     private static final String[] ADMIN_PERMIT_URLS = {
             "/api/question/**",
+            "/api/email/**",
+            "/api/redis/**",
             "/api/score_record/**",
             "/api/user_info/**",
             "/api/admin/**",
             "/question/**",
             "/score_record/**",
-            "/user_info/**"
+            "/user_info/**",
+            "/admin/**",
+            "/error/**",
+            "/manage/**"
     };
 
     @Bean
@@ -48,8 +56,19 @@ public class WebSecurityConfiguration
     }
 
     @Bean
-    SecurityFilterChain filterChain(HttpSecurity http) throws Exception
+    public SecurityFilterChain
+    filterChain(HttpSecurity http) throws Exception
     {
+//         http.addFilterBefore(
+//                 new JsonAuthenticationFilter(),
+//                 UsernamePasswordAuthenticationFilter.class
+//         );
+//
+//        // 使用 Cookie 存储 CSRF 令牌
+//        http.csrf(csrf -> csrf
+//                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+//                );
+//
 //         http.authorizeHttpRequests(
 //                (auth) ->
 //                        auth.requestMatchers(PUBLIC_URLS).permitAll()
@@ -59,15 +78,18 @@ public class WebSecurityConfiguration
 //        ).formLogin(
 //                (form) ->
 //                        form.loginPage("/user_info/login")
+//                            .loginProcessingUrl("/api/user_info/login")             // 指定登录表单提交的URL
+//                            .defaultSuccessUrl("/user_info/user_front_page", true)
 //                            .permitAll()
 //         ).exceptionHandling(
 //                 (handle) ->
 //                         handle.accessDeniedPage("/error/access_denied")
 //         );
 
-        http.authorizeHttpRequests((auth) ->
-                auth.requestMatchers("/**").permitAll()
-        ); //.csrf(AbstractHttpConfigurer::disable);
+        http.authorizeHttpRequests(
+                (auth) ->
+                        auth.requestMatchers("/**").permitAll()
+        );
 
         return http.build();
     }
