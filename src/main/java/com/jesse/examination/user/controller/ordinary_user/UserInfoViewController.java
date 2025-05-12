1d0f4a2dbdc73714ad582b03c1d2af10f858cae9
浩进 朱 <3191955858@qq.com>
2025-05-12 21:23:15 +0800

    用户账户数据操作视图类。

--- src/main/java/com/jesse/examination/user/controller/ordinary_user/UserInfoViewController.java
+++ src/main/java/com/jesse/examination/user/controller/ordinary_user/UserInfoViewController.java
@@ -1,4 +1,4 @@
-package com.jesse.examination.user.controller;
+package com.jesse.examination.user.controller.ordinary_user;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
