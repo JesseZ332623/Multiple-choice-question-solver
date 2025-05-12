9e38e07ab0f46b4170982a953249641d73f36c12
浩进 朱 <3191955858@qq.com>
2025-05-12 20:42:27 +0800

    文件传输服务实现类。

--- src/main/java/com/jesse/examination/file/impl/FileTransferService.java
+++ src/main/java/com/jesse/examination/file/impl/FileTransferService.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jesse.examination.config.jacksonconfig.JacksonConfig;
 import com.jesse.examination.file.FileTransferServiceInterface;
+import com.jesse.examination.file.exceptions.FileNotExistException;
 import com.jesse.examination.question.dto.QuestionCorrectTimesDTO;
 import com.jesse.examination.scorerecord.entity.ScoreRecordEntity;
 import lombok.extern.slf4j.Slf4j;
@@ -15,9 +16,7 @@ import org.springframework.util.FileSystemUtils;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.nio.file.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -33,6 +32,34 @@ public class FileTransferService implements FileTransferServiceInterface
 
     // 创建带有时间字符串解析的 ObjectMapper 实例。
     private final ObjectMapper mapper = JacksonConfig.createObjectMapper();
+
+    private static final byte[] DEFAULT_AVATAR_IMAGE_DATA;
+
+    // 静态块还可以这么用？
+    static
+    {
+        try
+        {
+            DEFAULT_AVATAR_IMAGE_DATA = Files.readAllBytes(
+                    Paths.get(
+                            "D:\\Spring-In-Action\\Multiple-choice-question-solver\\src\\main\\resources\\image\\avatar.png"
+                    )
+            );
+        }
+        catch (IOException exception)
+        {
+            log.error(exception.getMessage());
+            throw new RuntimeException(exception);
+        }
+    }
+
+    /**
+     * 获取默认头像图片的字节数组。
+     */
+    public static byte[] getDefaultAvatarImageData()
+    {
+        return DEFAULT_AVATAR_IMAGE_DATA;
+    }
 
     /**
      * 使用 Jackson 库，将 correct_times.json 文件中的一行 JSON 文本映射成一个
@@ -92,8 +119,58 @@ public class FileTransferService implements FileTransferServiceInterface
         return Objects.requireNonNull(termpScoreRecordEntity);
     }
 
+    private void
+    renameDirectory(String oldPathName, String newPathName) throws Exception
+    {
+        Path oldPath = Paths.get(oldPathName);
+        Path newPath = Paths.get(newPathName);
+
+        if (!Files.exists(oldPath))
+        {
+            throw new IllegalArgumentException(
+                    format("source path: %s not exist!", oldPathName)
+            );
+        }
+
+        try
+        {
+            Files.createDirectories(newPath.getParent());
+
+            Files.move(
+                    oldPath, newPath,
+                    StandardCopyOption.REPLACE_EXISTING,
+                    StandardCopyOption.ATOMIC_MOVE
+            );
+
+            log.info("Rename directory [{}] -> [{}].", oldPathName, newPathName);
+        }
+        catch (FileAlreadyExistsException exception)
+        {
+            String errorMessage = format(
+                    "New directory already exist, cause: %s",
+                    exception.getMessage()
+            );
+
+            log.error(errorMessage);
+
+            throw new Exception(errorMessage);
+        }
+        catch (IOException exception)
+        {
+            String errorMessage = format(
+                    "[%s] Failed operator cause: %s",
+                    exception.getClass().getSimpleName(),
+                    exception.getMessage()
+            );
+
+            log.error(errorMessage);
+
+            throw new Exception(errorMessage);
+        }
+    }
+
     /**
-     * 文件存储通用方法。
+     * 文本文件存储通用方法。
      *
      * @param filePath 文件路径
      * @param fileName 文件名
@@ -113,6 +190,80 @@ public class FileTransferService implements FileTransferServiceInterface
 
         // 写入数据
         Files.writeString(completeFilePath, fileData);
+    }
+
+    /**
+     * 非文本文件存储通用方法。
+     *
+     * @param filePath 文件路径
+     * @param fileName 文件名
+     * @param fileByteData 文件数据（这里是字节数组）
+     */
+    private void
+    saveFile(String filePath, String fileName, byte[] fileByteData) throws IOException
+    {
+        Path dir = Paths.get(filePath);
+
+
+        // 检查路径是否存在，没有则创建。
+        if (!Files.exists(dir)) {
+            Files.createDirectories(dir);
+        }
+
+        // 创建文件
+        Path completeFilePath = dir.resolve(fileName);
+
+        // 将数据写入文件
+        Files.write(completeFilePath, fileByteData);
+    }
+
+    /**
+     * 读取指定用户存档的头像数据（以字节数组的形式返回）。
+     */
+    @Override
+    public byte[] getUserAvatarImage(String userName) throws IOException
+    {
+        // 头像存储路径
+        String avatarPath = this.storagePath + "/" + userName + "/" + "avatar.png";
+
+        Path image = Paths.get(avatarPath);
+
+        if (!Files.exists(image))
+        {
+            throw new FileNotExistException(
+                    "[FileNotExistException] Path: " + avatarPath + " NOT EXIST!"
+            );
+        }
+
+        return Files.readAllBytes(image);
+    }
+
+    /**
+     * 将头像数据写入指定用户的存档。
+     */
+    @Override
+    public void writeUserAvatarImage(String userName, byte[] imageBytes) throws IOException
+    {
+        String avatarDirStr  = this.storagePath + "/" + userName + "/";
+        String avatarName    = "avatar.png";
+
+        Path avatarDir = Paths.get(avatarDirStr);
+
+        if (!Files.exists(avatarDir)) {
+            Files.createDirectories(avatarDir);
+        }
+
+        this.saveFile(avatarDirStr, avatarName, imageBytes);
+    }
+
+    @Override
+    public void
+    renameUserArchiveDir(String oldUserName, String newUserName) throws Exception
+    {
+        this.renameDirectory(
+                this.storagePath  + "/" + oldUserName,
+                this.storagePath + "/" + newUserName
+        );
     }
 
     /**
