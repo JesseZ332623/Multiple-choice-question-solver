62866e1e94a2d9a384d98d570259983f123be95a
浩进 朱 <3191955858@qq.com>
2025-05-12 20:42:18 +0800

    文件传输服务接口。

--- src/main/java/com/jesse/examination/file/FileTransferServiceInterface.java
+++ src/main/java/com/jesse/examination/file/FileTransferServiceInterface.java
@@ -4,6 +4,7 @@ import com.jesse.examination.question.dto.QuestionCorrectTimesDTO;
 import com.jesse.examination.scorerecord.entity.ScoreRecordEntity;
 import org.jetbrains.annotations.NotNull;
 
+import java.io.IOException;
 import java.util.List;
 
 /**
@@ -11,6 +12,21 @@ import java.util.List;
  */
 public interface FileTransferServiceInterface
 {
+    /**
+     * 读取指定用户存档的头像数据（以字节数组的形式返回）。
+     */
+    byte[] getUserAvatarImage(String userName) throws IOException;
+
+    /**
+     * 将头像数据写入指定用户的存档。
+     */
+    void writeUserAvatarImage(String userName, byte[] imageBytes) throws IOException;
+
+    /**
+     * 重命名用户存档路径（修改用户账户信息时用）。
+     */
+    void renameUserArchiveDir(String oldUserName, String newUserName) throws Exception;
+
     /**
      * 存储用户成绩记录文件。
      *
