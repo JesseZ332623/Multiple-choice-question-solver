fe43a61a7ab9470de835733f54d36a6c1686fdea
浩进 朱 <3191955858@qq.com>
2025-06-03 14:28:58 +0800

    邮件发送服务 Restful 控制器。

--- src/main/java/com/jesse/examination/email/controller/EmailSenderController.java
+++ src/main/java/com/jesse/examination/email/controller/EmailSenderController.java
@@ -1,7 +1,11 @@
 package com.jesse.examination.email.controller;
 
 import com.jesse.examination.email.dto.EmailContentDTO;
+import com.jesse.examination.email.entity.EmailAuthTableEntity;
+import com.jesse.examination.email.repo.EmailAuthTableRepo;
+import com.jesse.examination.email.service.EmailAuthServiceInterface;
 import com.jesse.examination.email.service.EmailSenderInterface;
+import com.jesse.examination.email.service.impl.EmailAuthService;
 import com.jesse.examination.email.service.impl.EmailSender;
 import com.jesse.examination.user.service.AdminServiceInterface;
 import lombok.extern.slf4j.Slf4j;
@@ -26,30 +30,32 @@ public class EmailSenderController
 {
     /** 默认的验证码长度是 8 位 */
     private final static int    DEFAULT_CODE_LENGTH      = 8;
-
     private final static String SMTP_HOST                = "smtp.qq.com";
     private final static int    SMTP_PORT                = 465;
 
     /**
-     * 未来验证码自然是由企业邮箱发出的，这里先用我的替代。
+     * 未来验证码自然是由企业邮箱发出的，
+     * 这里先用我的替代。
+     * 安全起见，这里显示的都是 key，值要从 redis 数据库里查，不能明文放出来。
      */
-    private final static String ENTERPRISE_EMAIL_ADDRESS = "3191955858@qq.com";
+    private final static String ENTERPRISE_EMAIL_ADDRESS = "ENTERPRISE_EMAIL_ADDRESS";
 
     /**
-     * 来自邮箱服务提供的授权码
+     * 来自邮箱服务提供的授权码键。
      */
-    private final static String SERVICE_AUTH_CODE = "ttlzjkjjbldzdffj";
+    private final static String SERVICE_AUTH_CODE = "SERVICE_AUTH_CODE";
 
     /**
      * 规定用户验证码在 Redis 中对应的键的格式是：VerifyCodeFor[user-name]
-    */
+     */
     public final static String VERIFYCODE_KEY = "VerifyCodeFor";
 
     /** 验证码的有效期为 3 分钟。 */
     private final static int CODE_VALID_TIME = 3;
 
-    private final EmailSenderInterface  emailSender;
-    private final AdminServiceInterface emailQueryService;
+    private final EmailAuthServiceInterface emailAuthService;
+    private final EmailSenderInterface      emailSender;
+    private final AdminServiceInterface     emailQueryService;
 
     private final RedisTemplate<String, Object> redisTemplate;
 
@@ -77,24 +83,55 @@ public class EmailSenderController
         return emailContent;
     }
 
+    /**
+     *  在构造函数完成相应注入操作后，
+     *  就直接把邮箱的发送者和授权码从数据库中读出，存入 Redis。
+     * （这个只是安全措施，我能保证数据库中一定有对应的数据。）
+     */
+    private void saveEmailAuthInfoToRedis()
+    {
+        EmailAuthTableEntity emailAuth
+                = this.emailAuthService.getAuthInfoById(1);
+
+        if (
+                this.redisTemplate.opsForValue().get(ENTERPRISE_EMAIL_ADDRESS) != null &&
+                this.redisTemplate.opsForValue().get(SERVICE_AUTH_CODE) != null
+        )
+        {
+            // 往 Redis 中存入发送者的邮箱
+            this.redisTemplate.opsForValue().set(
+                    ENTERPRISE_EMAIL_ADDRESS, emailAuth.getEmail()
+            );
+
+            // 往 Redis 中存入发送者的授权码
+            this.redisTemplate.opsForValue().set(
+                    SERVICE_AUTH_CODE, emailAuth.getEmailAuthCode()
+            );
+        }
+    }
+
     @Autowired
     public EmailSenderController(
+            EmailAuthServiceInterface      emailAuthService,
             AdminServiceInterface          emailQueryService,
             RedisTemplate<String, Object>  redisTemplate
     )
     {
+        this.emailAuthService   = emailAuthService;
+        this.emailQueryService  = emailQueryService;
+        this.redisTemplate      = redisTemplate;
+
+        this.saveEmailAuthInfoToRedis();
+
         // 邮箱模块采用生成器模式，所以它的参数需要我自己决定。
         this.emailSender = new EmailSender.EmailSenderBuilder()
-                                          .smtpHost(SMTP_HOST)
-                                          .smtpPort(SMTP_PORT)
-                                          .userName(ENTERPRISE_EMAIL_ADDRESS)
-                                          .appPassword(SERVICE_AUTH_CODE)
-                                          .defaultSetProperties()
-                                          .defaultSetSession()
-                                          .build();
-
-        this.emailQueryService = emailQueryService;
-        this.redisTemplate     = redisTemplate;
+                .smtpHost(SMTP_HOST)
+                .smtpPort(SMTP_PORT)
+                .userName((String) this.redisTemplate.opsForValue().get(ENTERPRISE_EMAIL_ADDRESS))
+                .appPassword((String) this.redisTemplate.opsForValue().get(SERVICE_AUTH_CODE))
+                .defaultSetProperties()
+                .defaultSetSession()
+                .build();
     }
 
     /**
@@ -113,12 +150,23 @@ public class EmailSenderController
 
             String varifyCode = EmailSender.generateVarifyCode(DEFAULT_CODE_LENGTH);
 
+            String entireVarifyKey = VERIFYCODE_KEY + userName;
+
+            /*
+             * 若在用户发送验证码前，上一回的验证码存在，需要删除。
+             *
+             */
+            if (this.redisTemplate.opsForValue().get(entireVarifyKey) != null)
+            {
+                this.redisTemplate.delete(entireVarifyKey);
+            }
+
             /*
              * 将验证码存入 Redis 数据库，
              * 并设置有效期为 CODE_VALID_TIME 分钟，超过该时间 Redis 会自动删除。
              */
             this.redisTemplate.opsForValue().set(
-                    VERIFYCODE_KEY + userName, varifyCode,
+                    entireVarifyKey, varifyCode,
                     CODE_VALID_TIME, TimeUnit.MINUTES
             );
 
