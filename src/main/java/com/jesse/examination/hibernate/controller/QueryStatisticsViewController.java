ce4211eabd1996b6d4aae875a7b276216d0f15e1
浩进 朱 <3191955858@qq.com>
2025-06-06 16:47:19 +0800

    Hibernate 查询统计视图控制器。

--- src/main/java/com/jesse/examination/hibernate/controller/QueryStatisticsViewController.java
+++ src/main/java/com/jesse/examination/hibernate/controller/QueryStatisticsViewController.java
@@ -1,4 +1,4 @@
-package com.jesse.examination.hibernate;
+package com.jesse.examination.hibernate.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
