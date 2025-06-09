616a57c2462219f1adf50eb0a7befa34066c45f1
浩进 朱 <3191955858@qq.com>
2025-06-09 17:18:02 +0800

    练习成绩记录服务类接口。

--- src/main/java/com/jesse/examination/scorerecord/service/ScoreRecordService.java
+++ src/main/java/com/jesse/examination/scorerecord/service/ScoreRecordService.java
@@ -4,10 +4,8 @@ import com.jesse.examination.scorerecord.entity.ScoreRecordEntity;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface ScoreRecordService
 {
