beff7e79384884532603ac6cb4c29c4482b04f84
浩进 朱 <3191955858@qq.com>
2025-06-12 17:27:59 +0800

    管理员用户操作服务接口。

--- src/main/java/com/jesse/examination/user/service/AdminServiceInterface.java
+++ src/main/java/com/jesse/examination/user/service/AdminServiceInterface.java
@@ -2,13 +2,21 @@ package com.jesse.examination.user.service;
 
 import com.jesse.examination.user.dto.admindto.AdminAddNewUserDTO;
 import com.jesse.examination.user.dto.admindto.AdminModifyUserDTO;
+import com.jesse.examination.user.dto.userdto.UserLoginDTO;
 import com.jesse.examination.user.entity.UserEntity;
+import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
 import java.util.List;
 
 public interface AdminServiceInterface
 {
+    /* 管理员登录 */
+    void adminUserLogin(@NotNull UserLoginDTO userLoginDTO);
+
+    /* 管理员登出 */
+    void adminUserLogout(String userName);
+
     UserEntity findUserByUserName(String userName);
 
     List<UserEntity> findAllUsers();
