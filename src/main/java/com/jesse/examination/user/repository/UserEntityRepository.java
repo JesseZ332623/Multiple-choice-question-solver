48ee5fb20df0c0344a03bd3bd5755a20b568acaa
浩进 朱 <3191955858@qq.com>
2025-06-13 14:55:57 +0800

    用户实体仓库接口类。

--- src/main/java/com/jesse/examination/user/repository/UserEntityRepository.java
+++ src/main/java/com/jesse/examination/user/repository/UserEntityRepository.java
@@ -12,8 +12,18 @@ public interface UserEntityRepository extends JpaRepository<UserEntity, Long>
      */
     Optional<UserEntity> findUserByUsername(String userName);
 
+    /**
+     * 通过用户名删除用户实体实体。（JPA 自动实现）
+     */
     void deleteUserByUsername(String userName);
 
-    boolean existsByFullName(String fullName);
+    /**
+     * 检查指定的用户名字段十否存在。（JPA 自动实现）
+     */
     boolean existsByUsername(String userName);
+
+    /**
+     * 检查指定的用户全名字段十否存在。（JPA 自动实现）
+     */
+    boolean existsByFullName(String fullName);
 }
