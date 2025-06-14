60cd89acd619c0c97247b46b6c466a700f031427
Jesse <3191955858@qq.com>
2025-06-14 20:35:18 +0800

    用户账户数据操作视图控制器。

--- src/main/java/com/jesse/examination/user/controller/ordinary_user/UserInfoViewController.java
+++ src/main/java/com/jesse/examination/user/controller/ordinary_user/UserInfoViewController.java
@@ -1,6 +1,9 @@
 package com.jesse.examination.user.controller.ordinary_user;
 
+import com.jesse.examination.errorhandle.ControllerErrorMessage;
+import com.jesse.examination.errorhandle.ErrorMessageGenerator;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,17 +20,73 @@ public class UserInfoViewController
     String loginView() { return "UserAccountPage/UserLogin"; }
 
     @GetMapping(path = "/modify")
-    String modifyView() { return "UserAccountPage/UserModify"; }
+    String modifyView(HttpServletRequest request, Model model)
+    {
+        try
+        {
+            HttpSession session = request.getSession(false);
+
+            if (session != null && session.getAttribute("user") != null) {
+                return "UserAccountPage/UserModify";
+            }
+            else
+            {
+                throw new RuntimeException(
+                        "Not login! can't preview this page!"
+                );
+            }
+        }
+        catch (Exception exception)
+        {
+            ControllerErrorMessage errorMessage
+                    = ErrorMessageGenerator.getErrorMessage(
+                    this.getClass().getSimpleName(),
+                    "modifyView",
+                    exception.getMessage()
+            );
+
+            model.addAttribute("ErrorMessage", errorMessage);
+
+            return "ErrorPage/Controller_ErrorPage";
+        }
+    }
 
     @GetMapping(path = "/delete")
     String deleteView(HttpServletRequest request, Model model)
     {
-        model.addAttribute(
-                "UserName",
-                (String) request.getSession(false)
-                                .getAttribute("user")
-        );
+        try
+        {
+            HttpSession session = request.getSession(false);
 
-        return "UserAccountPage/UserDelete";
+            if (session != null && session.getAttribute("user") != null)
+            {
+                model.addAttribute(
+                        "UserName",
+                        (String) request.getSession(false)
+                                .getAttribute("user")
+                );
+
+                return "UserAccountPage/UserDelete";
+            }
+            else
+            {
+                throw new RuntimeException(
+                        "Not login! can't preview this page!"
+                );
+            }
+        }
+        catch (Exception exception)
+        {
+            ControllerErrorMessage errorMessage
+                    = ErrorMessageGenerator.getErrorMessage(
+                    this.getClass().getSimpleName(),
+                    "deleteView",
+                    exception.getMessage()
+            );
+
+            model.addAttribute("ErrorMessage", errorMessage);
+
+            return "ErrorPage/Controller_ErrorPage";
+        }
     }
 }
