7ea513ff73b6b56ab9d840b0bc01d64a3acddd66
Jesse <3191955858@qq.com>
2025-06-14 20:28:19 +0800

    控制器方法错误信息类。

--- src/main/java/com/jesse/examination/errorhandle/ErrorViewController.java
+++ src/main/java/com/jesse/examination/errorhandle/ErrorViewController.java
@@ -1,20 +1,22 @@
 package com.jesse.examination.errorhandle;
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping(path = "/error")
 public class ErrorViewController
 {
-    @GetMapping(path = "/access_denied")
-    String accessDeniedView() {
+    @GetMapping(path = "/my_error/access_denied")
+    public String accessDeniedView() {
         return "ErrorPage/AccessDenied";
     }
 
-    @GetMapping(path = "/controller_error")
-    String controllerErrorView() {
+    @GetMapping(path = "/my_error/controller_error")
+    public String controllerErrorView() {
         return "ErrorPage/Controller_ErrorPage";
     }
 }
