ad1e5c0d677819f204cc2145e14ba9609d56ff57
浩进 朱 <3191955858@qq.com>
2025-06-19 13:48:30 +0800

    选择题问题仓库类。

--- src/main/java/com/jesse/examination/question/repository/QuestionRepository.java
+++ src/main/java/com/jesse/examination/question/repository/QuestionRepository.java
@@ -12,40 +12,27 @@ public interface QuestionRepository extends JpaRepository<QuestionEntity, Intege
 {
     /**
      * 查询题目，以及它的所有选项。
-     * 由于涉及到多表查询，所以需要使用 @Query 注解显式的编写 SQL 语句，
-     * 其中 SQL 查询语句的具体解释如下：
-     * <li>
-     * SELECT 关键字选择 </br>
-     * questions.id, questions.content, questions.answer 和
-     * JSON_OBJECTAGG(options.option_key, options.content)  </br>
-     * 四个字段，其中 JSON_OBJECTAGG() 是一个 SQL 聚合函数，
-     * 它将每个题目所有的选项 (options.option_key) 和 选项的内容 (options.content)
-     * 一一对应，组合成一个 JSON 对象，该函数仅限 MySQL 5.7+ 版本使用。
-     * </li>
+     * 由于涉及到多表查询，所以需要使用 @Query 注解显式的编写 SQL 语句。
+     * <ul>
+     *     <li>
+     *          其中 <code>JSON_OBJECTAGG()</code> 是一个 SQL 聚合函数，
+     *          它将每个题目所有的选项 (options.option_key) 和 选项的内容 (options.content)
+     *          一一对应，组合成一个 JSON 对象，该函数仅限 MySQL 5.7+ 版本使用。
+     *      </li>
+     * </ul>
      *
-     * <li>
-     * FROM + INNER JOIN 关键字进行多表关联 </br>
-     * INNER JOIN 关键字将 questions 表和 options 表通过外键 question_id 进行关联。 </br>
-     * 并且确保只有有选项的题目才会出现在结果之中。
-     * </li>
-     *
-     * <li>
-     * GROUP BY 关键字进行分组，本处是依照 questions.id 进行分组。
-     * </li>
-     * </br>
-     * 因此，这个 SQL 语句的功能就是：
-     * 查询每个选择题的题目、正确答案，答对次数及其所有的选项，
-     * 选项和内容需要整合到一个 JSON 之中。
-     * </br>
-     * <p>
-     * Tips: @Query 注解的 nativeQuery 属性表明是否为本机查询，默认为 false。
+     * <strong>
+     *     Tips: @Query 注解的 nativeQuery 属性表明是否为原生 SQL 查询，
+     *     默认为 false ，表示将传入的 SQL 语句按 JPA 的 JPQL 的语法进行解析，
+     *     反之则按 SQL 的语法进行解析。
+     * </strong>
      */
     @Query(
             value = """
                     SELECT
-                        questions.id            AS QuestionID,
-                        questions.content       AS QuestionContent,
-                        questions.answer        AS CorrectAnswer,
+                        questions.id      AS QuestionID,
+                        questions.content AS QuestionContent,
+                        questions.answer  AS CorrectAnswer,
                         JSON_OBJECTAGG(options.option_key, options.content) AS OptionsJSON
                     FROM questions
                     INNER JOIN options ON questions.id = options.question_id
@@ -60,10 +47,10 @@ public interface QuestionRepository extends JpaRepository<QuestionEntity, Intege
      */
     @Query(
             value = """
-                    SELECT questions.id      AS questionId, 
-                           questions.content AS questionContent, 
+                    SELECT questions.id      AS questionId,
+                           questions.content AS questionContent,
                            questions.answer  AS correctAnswer,
-                           options.content   AS correctOptionContent 
+                           options.content   AS correctOptionContent
                     FROM questions
                     INNER JOIN options
                     ON questions.id = options.question_id
