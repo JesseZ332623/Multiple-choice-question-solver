92f18854c6566f4d3812a56bd10fa346aa31fb96
浩进 朱 <3191955858@qq.com>
2025-06-19 14:04:59 +0800

    管理员用户操作视图控制器。（重构 columnValueProcess 方法）

--- src/main/java/com/jesse/examination/user/controller/admin/AdminViewController.java
+++ src/main/java/com/jesse/examination/user/controller/admin/AdminViewController.java
@@ -2,7 +2,7 @@ package com.jesse.examination.user.controller.admin;
 
 import com.jesse.examination.errorhandle.ControllerErrorMessage;
 import com.jesse.examination.errorhandle.ErrorMessageGenerator;
-import com.jesse.examination.user.controller.utils.UserInfoProcessUtils;
+import com.jesse.examination.user.controller.utils.CookieRoles;
 import com.jesse.examination.user.dto.admindto.AdminDisplayUsersDTO;
 import com.jesse.examination.user.entity.UserEntity;
 import com.jesse.examination.user.service.AdminServiceInterface;
@@ -17,10 +17,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Controller
@@ -35,16 +33,39 @@ public class AdminViewController
     }
 
     /**
-     * 对每一个用户实体稍作处理，密码仅仅显示加密后的前 6 位。
+     * 对每一个用户实体稍作处理，
+     * 密码仅仅显示加密后的前 6 位，以及更好地展示用户角色和登录时间数据。
+     *
+     * <p>2025-6-16: 从原来的迭代改成并行流的形式，在用户非常多的时候或许性能更好？</>
      */
-    private List<UserEntity>
+    private List<AdminDisplayUsersDTO>
     columnValueProcess(@NotNull List<UserEntity> allUsers)
     {
-        for (UserEntity user : allUsers) {
-            user.setPassword(user.getPassword().substring(0, 7));
-        }
+        // List<AdminDisplayUsersDTO> allDisplayUsers = new ArrayList<>();
 
-        return allUsers;
+        return allUsers.stream().parallel()
+        .map(
+                (user) -> {
+                    var adminDisplay = new AdminDisplayUsersDTO();
+                    adminDisplay.setId(user.getId());
+                    adminDisplay.setUserName(user.getUsername());
+                    adminDisplay.setPassword(user.getPassword().substring(0, 16));
+                    adminDisplay.setFullName(user.getFullName());
+                    adminDisplay.setTelephoneNumber(user.getTelephoneNumber());
+                    adminDisplay.setEmail(user.getEmail());
+                    adminDisplay.setRegisterDateTime(
+                            user.getRegisterDateTime()
+                                    .toString().replace('T', ' ')
+                    );
+                    adminDisplay.setRoles(
+                            AdminController.getRolesString(
+                                    user.getRoles()
+                            )
+                    );
+
+                    return adminDisplay;
+                }
+        ).collect(Collectors.toList());
     }
 
     @GetMapping(path = "all_users")
@@ -55,9 +76,9 @@ public class AdminViewController
         {
             HttpSession session  = request.getSession(false);
 
-            if (session != null && session.getAttribute("user") != null)
+            if (session != null && session.getAttribute(CookieRoles.ADMIN.toString()) != null)
             {
-                String loginUserName = (String) session.getAttribute("user");
+                String loginUserName = (String) session.getAttribute(CookieRoles.ADMIN.toString());
 
                 model.addAttribute("UserName", loginUserName);
                 model.addAttribute(
