2ba2b3c9329d96aeb64913892589b11fdd8153f4
浩进 朱 <3191955858@qq.com>
2025-06-19 14:00:51 +0800

    练习成绩记录仓库类接口。（重构所有 CURD 方法，部分采用 JPQL 实现）

--- src/main/java/com/jesse/examination/scorerecord/repository/ScoreRecordRepository.java
+++ src/main/java/com/jesse/examination/scorerecord/repository/ScoreRecordRepository.java
@@ -1,5 +1,6 @@
 package com.jesse.examination.scorerecord.repository;
 
+import com.jesse.examination.scorerecord.dto.ScoreRecordQueryDTO;
 import com.jesse.examination.scorerecord.entity.ScoreRecordEntity;
 
 import org.springframework.data.domain.Page;
@@ -8,8 +9,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import java.util.Optional;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface ScoreRecordRepository extends JpaRepository<ScoreRecordEntity, Integer>
@@ -18,16 +19,22 @@ public interface ScoreRecordRepository extends JpaRepository<ScoreRecordEntity, 
      * 查找指定 userName 的所有成绩记录，存于一个列表中。
      */
     @Query(
-            value = "SELECT * FROM score_record WHERE(user_name = :userName)",
+            value = """
+                    SELECT score_id, user_name, submit_date,
+                           correct_count, error_count, no_answer_count
+                    FROM score_record
+                    INNER JOIN users USING(user_id)
+                    WHERE user_name = :userName
+                    """,
             nativeQuery = true
     )
-    List<ScoreRecordEntity>
+    List<ScoreRecordQueryDTO>
     findAllScoreRecordByUserName(
             @Param(value = "userName") String userName
     );
 
     /**
-     * <p>分页的查找查找指定 userName 的所有成绩记录，存于一个列表中。</p>
+     * <p>分页的查找指定 userName 的所有成绩记录，存于一个列表中。</p>
      *
      * <p>
      *     这个 @Query 注解比较特殊，
@@ -36,14 +43,81 @@ public interface ScoreRecordRepository extends JpaRepository<ScoreRecordEntity, 
      */
     @Query(
             value = """
-                    SELECT s FROM ScoreRecordEntity s
-                    WHERE (s.userName = :userName)
-                    """
+                    SELECT NEW com.jesse.examination.scorerecord.dto.ScoreRecordQueryDTO(
+                        s.scoreId,
+                        u.username,
+                        s.submitDate,
+                        s.correctCount,
+                        s.errorCount,
+                        s.noAnswerCount
+                    )
+                    FROM ScoreRecordEntity  s
+                    JOIN s.userEntity       u
+                    WHERE u.username = :userName
+                    """,
+            countQuery = """
+                         SELECT COUNT(s)
+                         FROM ScoreRecordEntity  s
+                         JOIN s.userEntity       u
+                         WHERE u.username = :userName
+                         """
     )
-    Page<ScoreRecordEntity>
+    Page<ScoreRecordQueryDTO>
     findPaginatedScoreRecordByUserName(
             @Param(value = "userName") String userName,
             Pageable pageable
+    );
+
+    /**
+     * 找出指定用户最新的一条成绩记录。
+     */
+    @Query(value =  """
+            SELECT NEW com.jesse.examination.scorerecord.dto.ScoreRecordQueryDTO(
+                s.scoreId,
+                u.username,
+                s.submitDate,
+                s.correctCount,
+                s.errorCount,
+                s.noAnswerCount
+            )
+            FROM ScoreRecordEntity s
+            JOIN s.userEntity u
+            WHERE u.username = :userName
+            ORDER BY s.submitDate DESC
+            """,
+    countQuery = """
+                 SELECT COUNT(s)
+                 FROM ScoreRecordEntity s
+                 JOIN s.userEntity u
+                 WHERE u.username = :userName
+                 """)
+    Page<ScoreRecordQueryDTO>
+    findLatestScoreRecordByName(
+            @Param(value = "userName") String userName,
+            Pageable pageable
+    );
+
+    /**
+     * 插入指定用户 ID 的一条新成绩。
+     */
+    @Modifying
+    @Query(
+            value = """
+                    INSERT INTO score_record(
+                        user_id, submit_date,
+                        correct_count, error_count, no_answer_count
+                    )
+                    VALUE(
+                        :userId, :submitDate,
+                        :correctCount, :errorCount, :noAnswerCount
+                    )
+                    """,
+            nativeQuery = true
+    )
+    void insertNewScoreRecord(
+            Long userId, LocalDateTime submitDate,
+            Integer correctCount, Integer errorCount,
+            Integer noAnswerCount
     );
 
     /**
@@ -51,30 +125,17 @@ public interface ScoreRecordRepository extends JpaRepository<ScoreRecordEntity, 
      */
     @Modifying
     @Query(
-            value = "DELETE FROM score_record WHERE(user_name = :userName)",
+            value = """
+                    DELETE FROM score_record
+                    WHERE user_id IN (
+                        SELECT user_id FROM users
+                        WHERE user_name = :userName
+                    )
+                    """,
             nativeQuery = true
     )
     Integer deleteAllScoreRecordByUserName(
             @Param(value = "userName") String userName
-    );
-
-    /**
-     * 找出指定用户最新的一条成绩记录。
-     */
-    @Query(value = """
-                    SELECT * FROM score_record
-                    WHERE user_name = :userName
-                          AND
-                          submit_date = (
-                            SELECT MAX(submit_date)
-                            FROM   score_record
-                            WHERE  user_name = :userName
-                          )
-                   """,
-           nativeQuery = true)
-    Optional<ScoreRecordEntity>
-    findLatestScoreRecordByName(
-        @Param(value = "userName") String userName
     );
 
     /**
