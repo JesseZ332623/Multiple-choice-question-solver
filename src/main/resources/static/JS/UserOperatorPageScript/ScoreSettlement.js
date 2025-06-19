e8156e852f9e3705cd2aa9e7f90db395265032b2
浩进 朱 <3191955858@qq.com>
2025-06-19 13:36:20 +0800

    最新一次练习记录展示页面脚本。（计算而非存储错误率数据）

--- src/main/resources/static/JS/UserOperatorPageScript/ScoreSettlement.js
+++ src/main/resources/static/JS/UserOperatorPageScript/ScoreSettlement.js
@@ -4,10 +4,10 @@ async function getScoreSattlement()
 {
     try 
     {
-        const response = await fetch(
-            `/api/score_record/score_settlement/
-            ${document.getElementById('user_name_text').textContent.trim()}`
-        );
+        let requestURL 
+        = `/api/score_record/score_settlement/${document.getElementById('user_name_text').textContent.trim()}`;
+
+        const response = await fetch(requestURL);
 
         if (!response.ok) 
         {
@@ -21,16 +21,28 @@ async function getScoreSattlement()
 
         const responseJson = await response.json();
 
+        let questionAmount 
+                = Number.parseInt(
+                    document.getElementById('question_count').textContent
+                );
+        
+        // 错误率 =（（错误数 + 未答数）/ 总题数）* 100
+        let mistakeRate
+            = ((responseJson.errorCount + responseJson.noAnswerCount) / questionAmount * 100).toFixed(2);
+
         document.getElementById('correct_count').innerText 
                                     = `${responseJson.correctCount} 题`;
+
         document.getElementById('error_count').innerText
                                     = `${responseJson.errorCount} 题`;
+
         document.getElementById('no_answer_count').innerText
                                     = `${responseJson.noAnswerCount} 题`;
-        document.getElementById('mistake_rate').innerText           
-                                    = `${responseJson.mistakeRate} %`;
-        document.getElementById('combinded_mistake_rate').innerText
-                                    = `${responseJson.mistakeRate} %`;
+
+        document.getElementById('mistake_rate').innerText = `${mistakeRate} %`;
+
+        document.getElementById('combinded_mistake_rate').innerText = `${mistakeRate} %`;
+
         document.getElementById('submit_date_value').innerText
                                     = `${responseJson.submitDate}`;
     }
