d7c85c6964f06ec1670568d48b93e006431c6932
浩进 朱 <3191955858@qq.com>
2025-06-19 11:19:53 +0800

    控制器方法错误信息类。

--- src/main/java/com/jesse/examination/errorhandle/ErrorViewController.java
+++ src/main/java/com/jesse/examination/errorhandle/ErrorViewController.java
@@ -1,11 +1,7 @@
 package com.jesse.examination.errorhandle;
 
-import jakarta.servlet.http.HttpServletRequest;
-import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ErrorViewController
