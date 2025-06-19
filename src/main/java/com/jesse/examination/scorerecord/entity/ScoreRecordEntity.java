38a3c8a0e0127055fac43048ee2043eaeb409984
浩进 朱 <3191955858@qq.com>
2025-06-19 13:57:44 +0800

    score_record 数据表实体类。（表结构大改，增加 user_id 外键， 删除 mistake_rate 字段，这一数据将由计算得出）

--- src/main/java/com/jesse/examination/scorerecord/entity/ScoreRecordEntity.java
+++ src/main/java/com/jesse/examination/scorerecord/entity/ScoreRecordEntity.java
@@ -1,7 +1,6 @@
 package com.jesse.examination.scorerecord.entity;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.jesse.examination.user.entity.UserEntity;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -13,51 +12,43 @@ import java.time.LocalDateTime;
  * score_record 数据表实体类。
  */
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
 @Entity
 @Table(name = "score_record")
+@NoArgsConstructor
+@AllArgsConstructor
 public class ScoreRecordEntity
 {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "score_id")
-    private Integer scoreId;                // 成绩记录 ID
+    @Column(name = "score_id", unique = true, nullable = false)
+    private Integer scoreId;            // 成绩记录 ID
 
-    @Column(name = "user_name")
-    @JsonProperty("userName")
-    private String userName;                // 该条成绩属于的用户
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private UserEntity userEntity;      // 这些成绩属于用户表中的哪个用户（多对一）
 
-    @Column(name = "submit_date")
-    @JsonProperty("submitDate")
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
-    private LocalDateTime submitDate;       // 成绩提交日期
+    @Column(name = "submit_date", nullable = false)
+    private LocalDateTime submitDate;   // 成绩提交日期
 
-    @Column(name = "correct_count")
-    @JsonProperty("correctCount")
-    private Integer correctCount;           // 正确数
+    @Column(name = "correct_count", nullable = false)
+    private Integer correctCount;       // 正确数
 
-    @Column(name = "error_count")
-    @JsonProperty("errorCount")
-    private Integer errorCount;             // 错误数
+    @Column(name = "error_count", nullable = false)
+    private Integer errorCount;         // 错误数
 
-    @Column(name = "no_answer_count")
-    @JsonProperty("noAnswerCount")
-    private Integer noAnswerCount;          // 未答数
-
-    @Column(name = "mistake_rate")
-    @JsonProperty("mistakeRate")
-    private Double mistakeRate;             // 错误率
+    @Column(name = "no_answer_count", nullable = false)
+    private Integer noAnswerCount;      // 未答数
 
     @Override
     public String toString()
     {
         return String.format(
                 """
-                {"userName":"%s", "submitDate":"%s", "correctCount":%d, "errorCount":%d, "noAnswerCount":%d, "mistakeRate":%f}
+                Score ID = %d, User ID = %d, Submit Date = %s,
+                Correct Count = %d, Error Count = %d, No Answer Count = %d
                 """,
-                getUserName(), submitDate.toString(), getCorrectCount(),
-                getErrorCount(), getNoAnswerCount(), getMistakeRate()
+                scoreId, userEntity.getId(), submitDate.toString(),
+                correctCount, errorCount, noAnswerCount
         );
     }
 }
