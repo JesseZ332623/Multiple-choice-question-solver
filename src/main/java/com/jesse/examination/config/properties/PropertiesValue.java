9ffada597847d74beee62f6e33c87e53c7c0eabb
Jesse <3191955858@qq.com>
2025-06-22 18:50:37 +0800

    从 application.properties 中读取的属性值存于此处。

---
+++ src/main/java/com/jesse/examination/config/properties/PropertiesValue.java
@@ -0,0 +1,32 @@
+package com.jesse.examination.config.properties;
+
+import jakarta.validation.constraints.NotNull;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.Duration;
+
+@Component
+@Validated
+public class PropertiesValue
+{
+    @NotNull
+    @Value("${file.upload-dir}")
+    private String fileUploadPath;
+
+    @Value("${server.servlet.session.timeout}")
+    private Duration sessionTimeOut;
+
+    public Path getFileUploadPath()
+    {
+        return Paths.get(this.fileUploadPath)
+                    .toAbsolutePath().normalize();
+    }
+
+    public long getSessionTimeOutSeconds() {
+        return this.sessionTimeOut.toSeconds();
+    }
+}
