8350f74e700cd35d2689055caa7593ef451b3729
浩进 朱 <3191955858@qq.com>
2025-06-23 18:10:55 +0800

    邮件发送服务接口。（更新 sendEmail() 方法的返回类型）

--- src/main/java/com/jesse/examination/email/service/EmailSenderInterface.java
+++ src/main/java/com/jesse/examination/email/service/EmailSenderInterface.java
@@ -2,12 +2,18 @@ package com.jesse.examination.email.service;
 
 import com.jesse.examination.email.dto.EmailContentDTO;
 
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+
 public interface EmailSenderInterface
 {
     /**
      * 向指定用户发送邮件。
      *
      * @param emailContent 向指定用户发送邮件的数据传输类
+     *
+     * @return 一个期值，内部存储了方法执行是否成功的消息
      */
-    void sendEmail(EmailContentDTO emailContent);
+    CompletableFuture<Map.Entry<Boolean, String>>
+    sendEmail(EmailContentDTO emailContent);
 }
